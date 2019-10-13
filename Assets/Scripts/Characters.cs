9137c0432b98d71b794407175eb1f216d6d8abcf
Anders Dahlenborg <anders.dahlenborg@volvocars.com>
2019-10-13 10:39:56 +0200

    setting up

---
+++ Assets/Scripts/Characters.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Characters : MonoBehaviour
+{
+
+
+    protected Rigidbody2D body;
+    public float jumpForce = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    protected void Jump()
+    {
+        body.AddForce(new Vector2(0,jumpForce));
+    }
+
+
+
+
+    void Health()
+    {
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/Enemys.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemys : Characters 
+{
+
+    public float speed;
+    public float stoppingdistance;
+    public float retreatdistance;
+
+    public Transform player;
+
+    void Start()
+    {
+        
+
+        player = GameObject.FindGameObjectWithTag("herotag").transform;
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Vector2.Distance(transform.position, player.position) > stoppingdistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        } else if (Vector2.Distance(transform.position, player.position) < stoppingdistance && Vector2.Distance(transform.position, player.position)> retreatdistance) {
+
+            transform.position = this.transform.position;
+        } else if(Vector2.Distance(transform.position, player.position) < retreatdistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+
+
+
+    }
+}
